0c1c140b2123f9edfb2d83edd9c0c7a618749f9e
Matthew Holmes <89047848+tennisbun@users.noreply.github.com>
2023-02-26 01:15:16 -0500

    Add files via upload

---
+++ HCSR501.py
@@ -0,0 +1,16 @@
+
+from machine import Pin
+import time
+ 
+pir = Pin(21, Pin.IN, Pin.PULL_DOWN)
+n = 0
+ 
+print('Starting up the PIR Module')
+time.sleep(1)
+print('Ready')
+ 
+while True:
+     if pir.value() == 1:
+          n = n+1
+          print('Motion Detected ',n)
+     time.sleep(1)
