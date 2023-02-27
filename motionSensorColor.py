349e49be6c2ae91a7a1cb8382b4d34fa00a27eae
Matthew Holmes <89047848+tennisbun@users.noreply.github.com>
2023-02-27 01:08:27 -0500

    Update motionSensorColor.py

--- motionSensorColor.py
+++ motionSensorColor.py
@@ -11,7 +11,7 @@ pir = Pin(21, Pin.IN, Pin.PULL_DOWN)
 n = 0
 
 print('Starting up the PIR Module')
-time.sleep(1)
+time.sleep(0.3)
 print('Ready')
 
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
@@ -42,7 +42,7 @@ def pixels_show():
         dimmer_ar[i] = (g<<16) + (r<<8) + b
     sm.put(dimmer_ar, 8)
     time.sleep_ms(10)
-
+    
 def pixels_set(i, color):
     ar[i] = (color[1]<<16) + (color[0]<<8) + color[2]
 
@@ -50,9 +50,35 @@ def pixels_fill(color):
     for i in range(len(ar)):
         pixels_set(i, color)
 
+def wheel(pos):
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+        
+def rainbow_cycle():
+    for j in range(255):
+        for i in range(NUM_LEDS):
+            rc_index = (i * 256 // NUM_LEDS) + j
+            pixels_set(i, wheel(rc_index & 255))
+        pixels_show()
+
 CLEAR = (0,0,0)
 BLUE = (0,0,255)
-      
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+PURPLE = (180, 0, 255)
+WHITE = (255, 255, 255)
+COLORS = (RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE)
+
+rainbow_cycle()      
 pixels_fill(CLEAR)
 pixels_show()
 time.sleep(1)
@@ -70,4 +96,5 @@ while True:
         pixels_fill(CLEAR)
         pixels_show()
         time.sleep(0.5)
+        
 
